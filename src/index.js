f615f6c2d665d24d2394c0ce8f4d684134b0273c
annmora13 <annmoracp@gmail.com>
2023-04-17 13:09:59 -0400

    correcciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+/*function maskify(cardNumber) {
+  return cardNumber.slice(0, -4).replace(/./g, "#") + cardNumber.slice(-4);
+}
+  
+function (cardNumber) {
+  let sum = 0;
+  let isEven = false;
+  
+  for (let i = cardNumber.length - 1; i >= 0; i--) {
+    let digit = parseInt(cardNumber[i], 10);
+  
+    if (isEven) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+  
+    sum += digit;
+    isEven = !isEven;
+  }
+  
+  return sum % 10 === 0;
+}*/
+
+
+const validateButton= document.getElementById("validate") 
+validateButton.addEventListener('click',()=>{
+
+  const cardNumber = document.getElementById("cardNumber").value;
+
+  const maskedCardNumber = validator.maskify(cardNumber);
+
+  const isValid = validator.isValid(cardNumber);
+
+  const resultDiv = document.getElementById("result");
+
+  resultDiv.innerHTML = isValid
+    ? `El número de tarjeta ${maskedCardNumber} es válido.`
+    : `El número de tarjeta ${maskedCardNumber} no es válido.`;
+})
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,28 @@
-const validator = {
-  // ...
-};
+const validator= {
 
+  maskify: function(cardNumber) {
+    return cardNumber.slice(0, -4).replace(/./g, "#") + cardNumber.slice(-4);
+  },
+
+  isValid: function(cardNumber){
+    let sum = 0;
+    let isEven = false;
+
+    for (let i = cardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(cardNumber[i], 10);
+
+      if (isEven) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+
+      sum += digit;
+      isEven = !isEven;
+    }
+
+    return sum % 10 === 0;
+  }
+};
 export default validator;
